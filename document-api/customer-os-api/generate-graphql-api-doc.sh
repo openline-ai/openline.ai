3d1e7a77284e79881008b8b3a7e98d5ea6071ec5
mattbr0wn <matt@openline.ai>
2022-11-11 14:25:43 +0000

    update script to reflect new folder structure

--- document-api/customer-os-api/generate-graphql-api-doc.sh
+++ document-api/customer-os-api/generate-graphql-api-doc.sh
@@ -7,7 +7,7 @@ rm -rf public
 mkdir temp
 cd temp || exit
 git clone --branch otter https://github.com/openline-ai/openline-customer-os.git
-cp -r ./openline-customer-os/customer-os-api/server/graph/schemas ../data
+cp -r ./openline-customer-os/packages/server/customer-os-api/graph/schemas ../data
 cd ..
 
 yarn global add spectaql
