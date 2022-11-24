7a6912e7fe83a1098e8387fa55343f457f99299b
Jonty Knox <jonty@openline.ai>
2022-11-24 18:14:32 +0000

    Add dotenv, snowplow tracker and docs

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from '@theme-original/Footer';
 import type FooterType from '@theme/Footer';
 import type {WrapperProps} from '@docusaurus/types';
+import OpenlineTracker from '../../components/Tracker';
 import {WebChat} from "@openline-ai/openline-web-chat";
 import "@openline-ai/openline-web-chat/dist/esm/index.css"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
@@ -25,6 +26,15 @@ export default function FooterWrapper(props: Props): JSX.Element {
 
         ></WebChat>
       <Footer {...props} />
+      <OpenlineTracker
+                enabled={true}
+                appId={process.env.REACT_APP_SP_TRACKER_APP_ID}
+                trackerId={process.env.REACT_APP_SP_TRACKER_TRACKER_ID}
+                collectorUrl={process.env.REACT_APP_SP_TRACKER_COLLECTOR_URL}
+                bufferSize={process.env.REACT_APP_SP_TRACKER_BUFFER_SIZE}
+                minimumVisitLength={process.env.REACT_APP_SP_TRACKER_MIN_VISIT_SECONDS}
+                heartbeatDelay={process.env.REACT_APP_SP_TRACKER_HEARTBEAT_SECONDS}
+            />
     </>
   );
 }
