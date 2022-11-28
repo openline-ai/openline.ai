e08cc75700c7dccbb79e5f378ddefe6e03ca47f4
Gabriel Gontariu <gabi@openline.ai>
2022-11-28 11:57:37 +0200

    108-add-webchat-to-website

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import Footer from '@theme-original/Footer';
 import type FooterType from '@theme/Footer';
 import type {WrapperProps} from '@docusaurus/types';
+import {WebChat} from "@openline-ai/openline-web-chat";
+import "@openline-ai/openline-web-chat/dist/esm/index.css"
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 type Props = WrapperProps<typeof FooterType>;
 
 export default function FooterWrapper(props: Props): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
   return (
     <>
+        <WebChat apikey={`${siteConfig.customFields.REACT_APP_SP_API_KEY}`}
+                 httpServerPath={`${siteConfig.customFields.REACT_APP_SP_HTTP_PATH}`}
+                 wsServerPath={`${siteConfig.customFields.REACT_APP_SP_WS_PATH}`}
+                 trackerEnabled={`${siteConfig.customFields.REACT_APP_SP_TRACKER_ENABLED}` === 'true'}
+                 trackerAppId={`${siteConfig.customFields.REACT_APP_SP_TRACKER_APP_ID}`}
+                 trackerId={`${siteConfig.customFields.REACT_APP_SP_TRACKER_TRACKER_ID}`}
+                 trackerCollectorUrl={`${siteConfig.customFields.REACT_APP_SP_TRACKER_COLLECTOR_URL}`}
+                 trackerBufferSize={`${siteConfig.customFields.REACT_APP_SP_TRACKER_BUFFER_SIZE}`}
+                 trackerMinimumVisitLength={`${siteConfig.customFields.REACT_APP_SP_TRACKER_MIN_VISIT_SECONDS}`}
+                 trackerHeartbeatDelay={`${siteConfig.customFields.REACT_APP_SP_TRACKER_HEARTBEAT_SECONDS}`}
+
+        ></WebChat>
       <Footer {...props} />
     </>
   );
