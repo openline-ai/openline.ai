4d69e85e705a18de32f564c55d7e42d844d8596d
alexopenline <alex@openline.ai>
2023-01-09 09:32:43 +0200

    force deploy

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -22,7 +22,6 @@ export default function FooterWrapper(props: Props): JSX.Element {
                  trackerBufferSize={`${siteConfig.customFields.REACT_APP_SP_TRACKER_BUFFER_SIZE}`}
                  trackerMinimumVisitLength={`${siteConfig.customFields.REACT_APP_SP_TRACKER_MIN_VISIT_SECONDS}`}
                  trackerHeartbeatDelay={`${siteConfig.customFields.REACT_APP_SP_TRACKER_HEARTBEAT_SECONDS}`}
-
         ></WebChat>
       <Footer {...props} />
     </>
