2cb47ab674c2a305c8505df984cd0f1f697e8a26
Eduard Firut <edi@openline.ai>
2023-01-31 21:11:36 +0200

    Redeploy - properties changes

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -9,22 +9,22 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 type Props = WrapperProps<typeof FooterType>;
 
 export default function FooterWrapper(props: Props): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <>
-        <WebChat apikey={`${siteConfig.customFields.REACT_APP_SP_API_KEY}`}
-                 httpServerPath={`${siteConfig.customFields.REACT_APP_SP_HTTP_PATH}`}
-                 wsServerPath={`${siteConfig.customFields.REACT_APP_SP_WS_PATH}`}
-                 trackerEnabled={`${siteConfig.customFields.REACT_APP_SP_TRACKER_ENABLED}` === 'true'}
-                 trackerAppId={`${siteConfig.customFields.REACT_APP_SP_TRACKER_APP_ID}`}
-                 trackerId={`${siteConfig.customFields.REACT_APP_SP_TRACKER_TRACKER_ID}`}
-                 trackerCollectorUrl={`${siteConfig.customFields.REACT_APP_SP_TRACKER_COLLECTOR_URL}`}
-                 trackerBufferSize={`${siteConfig.customFields.REACT_APP_SP_TRACKER_BUFFER_SIZE}`}
-                 trackerMinimumVisitLength={`${siteConfig.customFields.REACT_APP_SP_TRACKER_MIN_VISIT_SECONDS}`}
-                 trackerHeartbeatDelay={`${siteConfig.customFields.REACT_APP_SP_TRACKER_HEARTBEAT_SECONDS}`}
-                 location='left'
-        ></WebChat>
-      <Footer {...props} />
-    </>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <>
+            <WebChat apikey={`${siteConfig.customFields.REACT_APP_SP_API_KEY}`}
+                     httpServerPath={`${siteConfig.customFields.REACT_APP_SP_HTTP_PATH}`}
+                     wsServerPath={`${siteConfig.customFields.REACT_APP_SP_WS_PATH}`}
+                     trackerEnabled={`${siteConfig.customFields.REACT_APP_SP_TRACKER_ENABLED}` === 'true'}
+                     trackerAppId={`${siteConfig.customFields.REACT_APP_SP_TRACKER_APP_ID}`}
+                     trackerId={`${siteConfig.customFields.REACT_APP_SP_TRACKER_TRACKER_ID}`}
+                     trackerCollectorUrl={`${siteConfig.customFields.REACT_APP_SP_TRACKER_COLLECTOR_URL}`}
+                     trackerBufferSize={`${siteConfig.customFields.REACT_APP_SP_TRACKER_BUFFER_SIZE}`}
+                     trackerMinimumVisitLength={`${siteConfig.customFields.REACT_APP_SP_TRACKER_MIN_VISIT_SECONDS}`}
+                     trackerHeartbeatDelay={`${siteConfig.customFields.REACT_APP_SP_TRACKER_HEARTBEAT_SECONDS}`}
+                     location='left'
+            ></WebChat>
+            <Footer {...props} />
+        </>
+    );
 }
